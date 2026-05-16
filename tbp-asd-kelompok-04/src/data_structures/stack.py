0470b932f5a073c94e61a4265cc76e5a63192da6
muhammad-farras-ashiddiq-64 <muhammadfarras.2025@student.uny.ac.id>
2026-05-16 21:03:02 +0700

    Update stack.py

--- tbp-asd-kelompok-04/src/data_structures/stack.py
+++ tbp-asd-kelompok-04/src/data_structures/stack.py
@@ -1,1 +1,20 @@
+from data_structures.linked_list import Node
 
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if not self.top:
+            return None
+        popped = self.top.data
+        self.top = self.top.next
+        return popped
+
+    def is_empty(self):
+        return self.top is None
