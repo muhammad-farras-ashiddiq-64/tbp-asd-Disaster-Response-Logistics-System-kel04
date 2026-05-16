99b3f90d1fcf4cf5aea630ecae73b7eaca7c3ff1
muhammad-farras-ashiddiq-64 <muhammadfarras.2025@student.uny.ac.id>
2026-05-17 02:16:30 +0700

    Update Modul_5_Dijkstra_dan_Audit_Jarak.py

--- tbp-asd-kelompok-04/src/modules/Modul_5_Dijkstra_dan_Audit_Jarak.py
+++ tbp-asd-kelompok-04/src/modules/Modul_5_Dijkstra_dan_Audit_Jarak.py
@@ -1,44 +1,11 @@
-from data_structures.linked_list import LinkedList
-
-def hitung_dijkstra(graph, start):
-    """Dijkstra O(V^2 + E) menghasilkan rute jarak minimum dari depot pusat"""
-    jarak = {node: float('inf') for node in graph.nodes}
-    jarak[start] = 0
-    unvisited = list(graph.nodes)
-    
-    while unvisited:
-        curr = min(unvisited, key=lambda n: jarak[n])
-        unvisited.remove(curr)
-        
-        if jarak[curr] == float('inf'):
-            break
-            
-        adj = graph.adj_list[curr]
-        while adj:
-            if adj.tujuan in unvisited:
-                path_alt = jarak[curr] + adj.bobot
-                if path_alt < jarak[adj.tujuan]:
-                    jarak[adj.tujuan] = path_alt
-            adj = adj.next
-    return jarak
-
-def audit_jarak_selection_sort(jarak_dict):
-    """Mengurutkan lokasi menggunakan Selection Sort pada Linked List manual"""
-    ll = LinkedList()
-    for nama, jarak in jarak_dict.items():
-        ll.append({"nama": nama, "jarak": jarak})
-        
-    # Proses Selection Sort pada Linked List
-    i = ll.head
-    while i:
-        min_node = i
-        j = i.next
-        while j:
-            if j.data["jarak"] < min_node.data["jarak"]:
-                min_node = j
-            j = j.next
-        # Tukar data objek
-        i.data, min_node.data = min_node.data, i.data
-        i = i.next
-        
-    return ll
+# Modul ini menangani fungsi pencetakan antarmuka CLI
+def cetak_bantuan_menu():
+    print("\n--- PERINTAH OPERASIONAL DISASTER RESPONSE LOGISTIC ---")
+    print("1. KIRIM <depot> <lokasi> <jenis> <jumlah>   : Enqueue armada")
+    print("2. PROSES_BANTUAN                            : Dequeue logistik terdepan")
+    print("3. RUTE_OPTIMAL <depot> <tujuan>             : Lintasan terpendek Dijkstra")
+    print("4. UPDATE_LEVEL <kode> <level>               : Ubah urgensi bencana (1-3)")
+    print("5. TIDAK_TERJANGKAU <depot>                  : Deteksi BFS wilayah terisolasi")
+    print("6. LOG_PENGIRIMAN                            : Tampilkan riwayat log (LIFO)")
+    print("7. LAPORAN_BENCANA                           : Cetak urutan kode (BST Inorder)")
+    print("8. KELUAR                                    : Keluar sistem")
