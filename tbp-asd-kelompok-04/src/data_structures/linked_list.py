b91587d5f7f65f3b87f1e78ee1fd64611b857f4f
muhammad-farras-ashiddiq-64 <muhammadfarras.2025@student.uny.ac.id>
2026-05-16 20:57:22 +0700

    Update linked_list.py

--- tbp-asd-kelompok-04/src/data_structures/linked_list.py
+++ tbp-asd-kelompok-04/src/data_structures/linked_list.py
@@ -1,1 +1,18 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
