9eae5636827c0eb6d6a4e01ecab1a7ffa93776de
muhammad-farras-ashiddiq-64 <muhammadfarras.2025@student.uny.ac.id>
2026-05-16 22:19:48 +0700

    Update Modul_3_BST_Registry_Lokasi.py

--- tbp-asd-kelompok-04/src/modules/Modul_3_BST_Registry_Lokasi.py
+++ tbp-asd-kelompok-04/src/modules/Modul_3_BST_Registry_Lokasi.py
@@ -1,1 +1,41 @@
+class BSTNode:
+    def __init__(self, kode, nama, level, populasi):
+        self.kode = kode  # Kunci/Key untuk BST
+        self.nama = nama
+        self.level = level
+        self.populasi = populasi
+        self.status = "Menunggu Bantuan"
+        self.left = None
+        self.right = None
 
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, kode, nama, level, populasi):
+        """O(log n) rata-rata"""
+        def _insert(node, kode, nama, level, populasi):
+            if not node:
+                return BSTNode(kode, nama, level, populasi)
+            if kode < node.kode:
+                node.left = _insert(node.left, kode, nama, level, populasi)
+            elif kode > node.kode:
+                node.right = _insert(node.right, kode, nama, level, populasi)
+            return node
+        self.root = _insert(self.root, kode, nama, level, populasi)
+
+    def search(self, kode):
+        """O(log n) rata-rata"""
+        curr = self.root
+        while curr:
+            if kode == curr.kode:
+                return curr
+            curr = curr.left if kode < curr.kode else curr.right
+        return None
+
+    def update_level(self, kode, level_baru):
+        node = self.search(kode)
+        if node:
+            node.level = level_baru
+            return True
+        return False
