6a24d983823ca59659fc16cceecfd118ea1b09ba
muhammad-farras-ashiddiq-64 <muhammadfarras.2025@student.uny.ac.id>
2026-05-17 19:48:10 +0530

    Update queue_ll.py

--- tbp-asd-kelompok-04/src/data_structures/queue_ll.py
+++ tbp-asd-kelompok-04/src/data_structures/queue_ll.py
@@ -1,39 +1,47 @@
-# src/data_structures/queue_ll.py
+class LLNode:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
-from data_structures.linked_list import LLNode
 
 class PriorityQueueBantuan:
+    """
+    Prioritas kecil = lebih penting
+    """
+
     def __init__(self):
         self.head = None
         self._size = 0
 
-    def enqueue(self, outbound_bantuan):
-        """Memasukkan data secara terurut berdasarkan prioritas. 
-        Angka prioritas lebih kecil (1 = Kritis) akan ditaruh di depan."""
-        new_node = LLNode(outbound_bantuan)
-        self._size += 1
-        
-        # Kasus 1: Antrean masih kosong ATAU data baru memiliki prioritas lebih tinggi dari head
-        if not self.head or outbound_bantuan.prioritas < self.head.data.prioritas:
+    def enqueue(self, bantuan):
+        new_node = LLNode(bantuan)
+
+        if not self.head or bantuan.prioritas < self.head.data.prioritas:
             new_node.next = self.head
             self.head = new_node
-            return
+        else:
+            current = self.head
 
-        # Kasus 2: Cari posisi penyisipan di tengah atau akhir antrean
-        curr = self.head
-        while curr.next and curr.next.data.prioritas <= outbound_bantuan.prioritas:
-            curr = curr.next
-        new_node.next = curr.next
-        curr.next = new_node
+            while (
+                current.next and
+                current.next.data.prioritas <= bantuan.prioritas
+            ):
+                current = current.next
+
+            new_node.next = current.next
+            current.next = new_node
+
+        self._size += 1
 
     def dequeue(self):
-        """Mengambil data terdepan (Prioritas Tertinggi) -> O(1)"""
         if not self.head:
             return None
-        temp = self.head.data
+
+        data = self.head.data
         self.head = self.head.next
         self._size -= 1
-        return temp
+
+        return data
 
     def __len__(self):
         return self._size
