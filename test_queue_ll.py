58b4bf04c5a1561e0e3717c4abd0067a7aa75d9f
muhammad-farras-ashiddiq-64 <muhammadfarras.2025@student.uny.ac.id>
2026-05-17 19:56:32 +0530

    Create test_queue_ll.py

---
+++ test_queue_ll.py
@@ -0,0 +1,107 @@
+=== Disaster Response Logistics System ===
+
+1. KIRIM
+2. PROSES_BANTUAN
+3. RUTE_OPTIMAL
+4. LOG_PENGIRIMAN
+5. KELUAR
+
+Pilih: 1
+Tujuan: L005
+Bantuan masuk antrian
+
+1. KIRIM
+2. PROSES_BANTUAN
+3. RUTE_OPTIMAL
+4. LOG_PENGIRIMAN
+5. KELUAR
+
+Pilih: 1
+Tujuan: L002
+Bantuan masuk antrian
+
+1. KIRIM
+2. PROSES_BANTUAN
+3. RUTE_OPTIMAL
+4. LOG_PENGIRIMAN
+5. KELUAR
+
+Pilih: 2
+
+Diproses:
+Bantuan(
+    bantuan_id=2,
+    jenis='MAKANAN',
+    jumlah=100,
+    asal='DEPOT_0',
+    tujuan='L002',
+    prioritas=1
+)
+
+1. KIRIM
+2. PROSES_BANTUAN
+3. RUTE_OPTIMAL
+4. LOG_PENGIRIMAN
+5. KELUAR
+
+Pilih: 2
+
+Diproses:
+Bantuan(
+    bantuan_id=1,
+    jenis='MAKANAN',
+    jumlah=100,
+    asal='DEPOT_0',
+    tujuan='L005',
+    prioritas=2
+)
+
+1. KIRIM
+2. PROSES_BANTUAN
+3. RUTE_OPTIMAL
+4. LOG_PENGIRIMAN
+5. KELUAR
+
+Pilih: 3
+Depot: DEPOT_0
+Tujuan: L005
+
+Jarak optimal: 84
+
+1. KIRIM
+2. PROSES_BANTUAN
+3. RUTE_OPTIMAL
+4. LOG_PENGIRIMAN
+5. KELUAR
+
+Pilih: 4
+
+=== LOG PENGIRIMAN ===
+
+Bantuan(
+    bantuan_id=1,
+    jenis='MAKANAN',
+    jumlah=100,
+    asal='DEPOT_0',
+    tujuan='L005',
+    prioritas=2
+)
+
+Bantuan(
+    bantuan_id=2,
+    jenis='MAKANAN',
+    jumlah=100,
+    asal='DEPOT_0',
+    tujuan='L002',
+    prioritas=1
+)
+
+1. KIRIM
+2. PROSES_BANTUAN
+3. RUTE_OPTIMAL
+4. LOG_PENGIRIMAN
+5. KELUAR
+
+Pilih: 5
+
+Program selesai.
