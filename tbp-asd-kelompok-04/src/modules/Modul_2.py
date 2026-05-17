8184d38f6186f57d4731277130c89945965ce1a0
muhammad-farras-ashiddiq-64 <muhammadfarras.2025@student.uny.ac.id>
2026-05-17 17:34:04 +0530

    Update Modul_2.py

--- tbp-asd-kelompok-04/src/modules/Modul_2.py
+++ tbp-asd-kelompok-04/src/modules/Modul_2.py
@@ -1,1 +1,46 @@
+# src/data_structures/queue_ll.py
 
+from data_structures.linked_list import LLNode
+
+class PriorityQueueBantuan:
+    def __init__(self):
+        self.head = None
+        self._size = 0
+
+    def enqueue(self, outbound_bantuan):
+        """
+        KIRIM: Menambahkan bantuan secara terurut ke dalam linked list.
+        Kompleksitas: O(n)
+        """
+        new_node = LLNode(outbound_bantuan)
+        self._size += 1
+        
+        # KONDISI 1: Antrean kosong atau bantuan baru memiliki prioritas lebih tinggi (angka lebih kecil) dari head
+        if not self.head or outbound_bantuan.prioritas < self.head.data.prioritas:
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        # KONDISI 2: Cari posisi penyisipan di tengah atau di ujung akhir (O(n) traversal)
+        curr = self.head
+        while curr.next and curr.next.data.prioritas <= outbound_bantuan.prioritas:
+            curr = curr.next
+            
+        new_node.next = curr.next
+        curr.next = new_node
+
+    def dequeue(self):
+        """
+        PROSES_BANTUAN: Mengambil armada logistik urutan terdepan.
+        Kompleksitas: O(1)
+        """
+        if not self.head:
+            return None
+            
+        temp_data = self.head.data
+        self.head = self.head.next  # Geser pointer head ke belakang
+        self._size -= 1
+        return temp_data
+
+    def __len__(self):
+        return self._size
