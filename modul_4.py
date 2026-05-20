78725cfcbecd59d1321b05f0e9a450dde520f0fa
Restu Pamungkas <restu3902ft.2025@student.uny.ac.id>
2026-05-20 20:18:53 +0700

    Update modul_4.py

--- modul_4.py
+++ modul_4.py
@@ -1,1 +1,34 @@
+from src.models import LLNode
 
+class Stack: 
+    def __init__(self): 
+        self.top = None 
+        self._size = 0 
+
+    def push(self, data): 
+        """Big-O O(1)"""
+        new_node = LLNode(data)
+        new_node.next = self.top
+        self.top = new_node
+        self._size += 1
+
+    def pop(self): 
+        """Big-O O(1)"""
+        if self.top is None:
+            return None
+        temp = self.top.data
+        self.top = self.top.next
+        self._size -= 1
+        return temp
+
+    def peek(self): 
+        return self.top.data if self.top else None 
+
+    def to_list(self): 
+        """Kembalikan list dari top ke bottom"""
+        result = []
+        current = self.top
+        while current:
+            result.append(current.data)
+            current = current.next
+        return result
