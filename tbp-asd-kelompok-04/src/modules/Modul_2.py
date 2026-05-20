771eeda45b31ab834dc0362cf800fd3264f70836
muhammad-farras-ashiddiq-64 <muhammadfarras.2025@student.uny.ac.id>
2026-05-20 17:50:08 +0700

    Update Modul_2.py

--- tbp-asd-kelompok-04/src/modules/Modul_2.py
+++ tbp-asd-kelompok-04/src/modules/Modul_2.py
@@ -1,68 +1,36 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+from src.models import Bantuan, LLNode
 
+class PriorityQueueBantuan: 
+    """Lokasi KRITIS (level=1) selalu dilayani lebih dulu.""" 
+    def __init__(self): 
+        self.head = None 
+        self._size = 0 
 
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear = None
-        self.size = 0
+    def enqueue(self, bantuan: Bantuan): 
+        """Big-O: O(n) insertion terurut prioritas angka terkecil (1=Kritis, dst).""" 
+        new_node = LLNode(bantuan)
+        self._size += 1
+        
+        if self.head is None or bantuan.prioritas < self.head.data.prioritas:
+            new_node.next = self.head
+            self.head = new_node
+            return
 
-    # O(1)
-    def enqueue(self, data):
-
-        new_node = Node(data)
-
-        if self.rear is None:
-            self.front = self.rear = new_node
-        else:
-            self.rear.next = new_node
-            self.rear = new_node
-
-        self.size += 1
-
-    # O(1)
-    def dequeue(self):
-
-        if self.front is None:
-            return None
-
-        temp = self.front
-        self.front = self.front.next
-
-        if self.front is None:
-            self.rear = None
-
-        self.size -= 1
-
-        return temp.data
-
-    # O(1)
-    def peek(self):
-
-        if self.front:
-            return self.front.data
-
-        return None
-
-    # O(1)
-    def is_empty(self):
-        return self.front is None
-
-    # O(1)
-    def __len__(self):
-        return self.size
-
-    # O(n)
-    def to_list(self):
-
-        result = []
-        current = self.front
-
-        while current:
-            result.append(current.data)
+        current = self.head
+        while current.next is not None and current.next.data.prioritas <= bantuan.prioritas:
             current = current.next
+        
+        new_node.next = current.next
+        current.next = new_node
 
-        return result
+    def dequeue(self): 
+        """Big-O: O(1). Ambil dari head.""" 
+        if self.head is None:
+            return None
+        temp = self.head.data
+        self.head = self.head.next
+        self._size -= 1
+        return temp
+
+    def __len__(self): 
+        return self._size
