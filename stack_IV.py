03ad5f5965f5569511f00cd755b0fb6a13107b19
Restu Pamungkas <restu3902ft.2025@student.uny.ac.id>
2026-05-20 20:27:07 +0700

    Update stack_IV.py

--- stack_IV.py
+++ stack_IV.py
@@ -1,1 +1,35 @@
+from src.data_structures.linked_list import LLNode
 
+class Stack:
+    """Stack Log Pengiriman untuk menampung riwayat transaksi (LIFO)."""
+    def __init__(self):
+        self.top = None
+        self._size = 0
+
+    def push(self, data):
+        """Big-O: O(1) - Menambahkan riwayat ke atas stack."""
+        new_node = LLNode(data)
+        new_node.next = self.top
+        self.top = new_node
+        self._size += 1
+
+    def pop(self):
+        """Big-O: O(1) - Mengambil log paling terakhir (untuk keperluan ROLLBACK)."""
+        if self.top is None:
+            return None
+        popped = self.top.data
+        self.top = self.top.next
+        self._size -= 1
+        return popped
+
+    def to_list(self):
+        """Big-O: O(n) - Mengonversi log ke bentuk list python standard untuk iterasi eksternal."""
+        result = []
+        current = self.top
+        while current:
+            result.append(current.data)
+            current = current.next
+        return result
+        
+    def __len__(self):
+        return self._size
